ce6567df5c9b914a0819b2baa0062e3055f43655
William Chan <wellkord@yahoo.com>
2022-10-11 23:00:33 +0800

    #12 Implement scheduler actions

--- src/BlazingQuartz/BlazingQuartz/Pages/BlazingQuartzUI/Overview/Overview.razor.cs
+++ src/BlazingQuartz/BlazingQuartz/Pages/BlazingQuartzUI/Overview/Overview.razor.cs
@@ -11,10 +11,14 @@ using MudBlazor;
 
 namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
 {
-    public partial class Overview : ComponentBase
+    public partial class Overview : ComponentBase, IDisposable
     {
         const string UptimeKey = "Uptime";
         const string StatusKey = "Status";
+        const string STARTED = "Started";
+        const string STARTING = "Starting";
+        const string STANDBY = "Standby";
+        const string SHUTDOWN = "Shutdown";
 
         static double[] EmptyData = { 0 };
         static string[] EmptyLabel = { $"{JobExecutionStatus.Success} (0)" };
@@ -22,18 +26,25 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
         [Inject] private IDialogService DialogSvc { get; set; } = null!;
         [Inject] IExecutionLogService LogSvc { get; set; } = null!;
         [Inject] ISchedulerService SchSvc { get; set; } = null!;
+        [Inject] ISchedulerListenerService SchLisSvc { get; set; } = null!;
+        [Inject] private ISnackbar Snackbar { get; set; } = null!;
 
         private MudTable<ExecutionLog> table = null!;
         private ExecutionLogFilter _errorExecutionLogFilter = new ExecutionLogFilter 
         {
-            ErrorOnly = true,
+            //ErrorOnly = true,
             LogTypes = new HashSet<LogType>{ LogType.ScheduleJob },
         };
-        private PageMetadata errorLogPage = new PageMetadata(1, 10);
-        private DateTimeOffset? _runningSince;
+        private PageMetadata errorLogPage = new PageMetadata(0, 10);
+        private DateTimeOffset? RunningSince;
 
         private List<ExecutionLog> ErrorLogs = new();
         private OrderedDictionary SchedulerInfo = new();
+
+        private bool IsPauseResumeDisabled = false;
+        private bool IsStartStandbyDisabled = false;
+        private bool IsStartButtonVisible = false;
+        private bool IsShutdown = false;
 
         #region charts
         private int JobCount;
@@ -63,24 +74,117 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
         private double[] AllTimeLogData = EmptyData;
         private string[] AllTimeLogLabel = EmptyLabel;
         private ChartOptions AllChartOptions = emptyExecutionChartOptions;
+
+        private DateTimeOffset lastCaptureDate = DateTimeOffset.Now.Date;
         #endregion charts
+
+        #region Refresh timer
+        private Timer? _refreshTimer;
+        private const int REFRESH_IN_MS = 10000;
+        private bool AutoRefresh = true;
+        #endregion Refresh timer
 
         protected override async Task OnInitializedAsync()
         {
+            await LoadInfo();
+            RegisterEventListeners();
+
             await Task.WhenAll(RefreshErrorLogs(),
-                LoadInfo(),
                 RefreshSchedulesCount(),
                 RefreshLogSummary(),
                 LoadYesterdaysLogSummary());
+
+            _refreshTimer = new Timer(async (_) =>
+            {
+                await InvokeAsync(async () =>
+                {
+                    //_logger.LogInformation("{time} Refresh trade summary", DateTime.Now);
+                    RefreshUptime();
+                    await Task.WhenAll(RefreshErrorLogs(),
+                        RefreshSchedulesCount(),
+                        RefreshLogSummary());
+
+                    // Update the UI
+                    StateHasChanged();
+                });
+            }, null,
+            IsPauseResumeDisabled ? Timeout.Infinite : REFRESH_IN_MS,
+            REFRESH_IN_MS);
+
+        }
+
+        private void UnRegisterEventListeners()
+        {
+            SchLisSvc.OnSchedulerInStandbyMode -= SchLisSvc_OnSchedulerInStandbyMode;
+            SchLisSvc.OnSchedulerShutdown -= SchLisSvc_OnSchedulerShutdown;
+            SchLisSvc.OnSchedulerStarted -= SchLisSvc_OnSchedulerStarted;
+            SchLisSvc.OnSchedulerStarting -= SchLisSvc_OnSchedulerStarting;
+        }
+
+        private void RegisterEventListeners()
+        {
+            SchLisSvc.OnSchedulerInStandbyMode += SchLisSvc_OnSchedulerInStandbyMode;
+            SchLisSvc.OnSchedulerShutdown += SchLisSvc_OnSchedulerShutdown;
+            SchLisSvc.OnSchedulerStarted += SchLisSvc_OnSchedulerStarted;
+            SchLisSvc.OnSchedulerStarting += SchLisSvc_OnSchedulerStarting;
+        }
+
+        private async void SchLisSvc_OnSchedulerStarting(object? sender, CancellationToken e)
+        {
+            await InvokeAsync(() =>
+            {
+                SchedulerInfo[StatusKey] = STARTING;
+                IsStartStandbyDisabled = true;
+
+                StateHasChanged();
+            });
+        }
+
+        private async void SchLisSvc_OnSchedulerStarted(object? sender, CancellationToken e)
+        {
+            await InvokeAsync(async () =>
+            {
+                Snackbar.Add("Scheduler started", Severity.Info);
+                await LoadInfo();
+                IsStartStandbyDisabled = false;
+                StartAutoRefresh();
+
+                StateHasChanged();
+            });
+        }
+
+        private async void SchLisSvc_OnSchedulerShutdown(object? sender, CancellationToken e)
+        {
+            await InvokeAsync(async () =>
+            {
+                Snackbar.Add("Scheduler was shutdown", Severity.Info);
+                await LoadInfo();
+                StopAutoRefresh();
+
+                StateHasChanged();
+            });
+        }
+
+        private async void SchLisSvc_OnSchedulerInStandbyMode(object? sender, CancellationToken e)
+        {
+            await InvokeAsync(async () =>
+            {
+                Snackbar.Add("Scheduler in standby mode", Severity.Info);
+                await LoadInfo();
+                StopAutoRefresh();
+
+                StateHasChanged();
+            });
+
         }
 
         private async Task RefreshLogSummary()
         {
             var todayDateUtc = DateTime.Now.Date.ToUniversalTime();
-            var yesterdayDateUtc = DateTime.Now.Date.AddDays(-1).ToUniversalTime();
             var today = await LogSvc.GetJobExecutionStatusSummary(
                 todayDateUtc);
             var allTime = await LogSvc.GetJobExecutionStatusSummary(null);
+            var nowDate = DateTimeOffset.Now.Date;
 
             if (!today.Data.Any())
             {
@@ -94,6 +198,11 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
                 TodaysLogData = chartData.Item1;
                 TodaysLogLabel = chartData.Item2;
                 TodaysChartOptions = executionChartOptions;
+            }
+
+            if (nowDate != lastCaptureDate)
+            {
+                await LoadYesterdaysLogSummary();
             }
 
             if (!allTime.Data.Any())
@@ -110,12 +219,16 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
                 AllTimeLogLabel = chartData.Item2;
                 AllChartOptions = executionChartOptions;
 
-                TotalLogDays = (int)Math.Round(DateTime.UtcNow.Subtract(allTime.StartDateTimeUtc).TotalDays);
+                TotalLogDays = (int)Math.Round(DateTime.Now.Subtract(
+                    DateTime.SpecifyKind(allTime.StartDateTimeUtc, DateTimeKind.Utc).ToLocalTime()).TotalDays);
             }
+
+            lastCaptureDate = nowDate;
         }
 
         /// <summary>
-        /// Yesterday's log summary. Pull this out from RefreshLogSummary() since this data won't change
+        /// Yesterday's log summary. Separated from <see cref="RefreshLogSummary"/> since only need to
+        /// call this when <see cref="lastCaptureDate"/> is different
         /// </summary>
         /// <returns></returns>
         private async Task LoadYesterdaysLogSummary()
@@ -124,7 +237,6 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
             var yesterdayDateUtc = DateTime.Now.Date.AddDays(-1).ToUniversalTime();
             var yesterday = await LogSvc.GetJobExecutionStatusSummary(
                 yesterdayDateUtc, todayDateUtc.AddMilliseconds(-1));
-
             if (!yesterday.Data.Any())
             {
                 YesterdaysLogData = EmptyData;
@@ -189,6 +301,12 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
             TriggerCount -= SysTriggerCount;
         }
 
+        private void RefreshUptime()
+        {
+            SchedulerInfo[UptimeKey] = RunningSince.HasValue ?
+                DateTimeOffset.UtcNow.Subtract(RunningSince.Value).ToHumanTimeString() : "--";
+        }
+
         private async Task LoadInfo()
         {
             var metadata = await SchSvc.GetMetadataAsync();
@@ -199,21 +317,28 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
 
             SchedulerInfo.Clear();
 
-            _runningSince = metadata.RunningSince;
+            RunningSince = metadata.RunningSince;
 
-            SchedulerInfo.Add(StatusKey, metadata.Started ? "Started" : 
-                (metadata.InStandbyMode ? "Standby" : 
-                    (metadata.Shutdown ? "Shutdown" : "Unknown")));
-            SchedulerInfo.Add(UptimeKey, _runningSince.HasValue ? 
-                DateTimeOffset.UtcNow.Subtract(_runningSince.Value).ToHumanTimeString() : "--");
+            IsStartButtonVisible = metadata.InStandbyMode || metadata.Shutdown;
+            IsShutdown = metadata.Shutdown;
+            IsPauseResumeDisabled = IsStartButtonVisible || IsShutdown;
+
+            SchedulerInfo.Add("Quartz Version", metadata.Version);
+            SchedulerInfo.Add("BlazingQuartz Version", typeof(Overview).Assembly.GetName().Version);
+            SchedulerInfo.Add(StatusKey, metadata.Shutdown ? SHUTDOWN : 
+                (metadata.InStandbyMode ? STANDBY : 
+                    (metadata.Started ? STARTED : "Unknown")));
+            SchedulerInfo.Add(UptimeKey, RunningSince.HasValue ? 
+                DateTimeOffset.UtcNow.Subtract(RunningSince.Value).ToHumanTimeString() : "--");
             SchedulerInfo.Add("Scheduler Instance Id", metadata.SchedulerInstanceId);
             SchedulerInfo.Add("Scheduler Name", metadata.SchedulerName);
             SchedulerInfo.Add("Scheduler Remote", metadata.SchedulerRemote);
             SchedulerInfo.Add("Scheduler Type", metadata.SchedulerType);
+            SchedulerInfo.Add("JobStore Type", metadata.JobStoreType);
+            SchedulerInfo.Add("Support Persistence", metadata.JobStoreSupportsPersistence);
+            SchedulerInfo.Add("Clustered", metadata.JobStoreClustered);
             SchedulerInfo.Add("Thread Pool Size", metadata.ThreadPoolSize);
             SchedulerInfo.Add("Thread Pool Type", metadata.ThreadPoolType);
-            SchedulerInfo.Add("Quartz Version", metadata.Version);
-            SchedulerInfo.Add("BlazingQuartz Version", typeof(Overview).Assembly.GetName().Version);
         }
 
         private void OnMoreDetails(ExecutionLog log, string title)
@@ -231,6 +356,80 @@ namespace BlazingQuartz.Pages.BlazingQuartzUI.Overview
             };
             DialogSvc.Show<ExecutionDetailsDialog>(title, parameters, options);
         }
+
+        public void Dispose()
+        {
+            _refreshTimer?.Dispose();
+            UnRegisterEventListeners();
+        }
+
+        #region Action buttons
+        private async Task OnStartScheduler()
+        {
+            await SchSvc.StartScheduler();
+        }
+
+        private async Task OnStandbyScheduler()
+        {
+            await SchSvc.StandbyScheduler();
+        }
+
+        private async Task OnShutdownScheduler()
+        {
+            await SchSvc.ShutdownScheduler();
+        }
+
+        private async Task OnPauseAllSchedules()
+        {
+            try
+            {
+                await SchSvc.PauseAllSchedules();
+                Snackbar.Add("Paused all schedules", Severity.Info);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add($"Error pausing all schedules. {ex.Message}", Severity.Error);
+            }
+        }
+
+        private async Task OnResumeAllSchedules()
+        {
+            try
+            {
+                await SchSvc.ResumeAllSchedules();
+                Snackbar.Add("Resumed all schedules", Severity.Info);
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add($"Error resuming all schedules. {ex.Message}", Severity.Error);
+            }
+        }
+        #endregion Action buttons
+
+        #region Auto refresh
+        private void StartAutoRefresh()
+        {
+            _refreshTimer?.Change(0, REFRESH_IN_MS);
+        }
+
+        private void StopAutoRefresh()
+        {
+            _refreshTimer?.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private void OnCheckAutoRefresh(bool flag)
+        {
+            AutoRefresh = flag;
+            if (AutoRefresh)
+            {
+                StartAutoRefresh();
+            }
+            else
+            {
+                StopAutoRefresh();
+            }
+        }
+        #endregion Auto refresh
     }
 }
 
